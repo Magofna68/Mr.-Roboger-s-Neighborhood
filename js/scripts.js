2332118ac920fdfab322d323638a59914862b92c
Brandon-Magofna <magofna68@gmail.com>
2021-04-02 15:39:34 -0700

    update README

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,15 @@
-// $(document).ready(function () {
-//   $("#number-input").submit(function (event)) {
-// event.preventDefault();
 
-//     ().onclick()
-// const numberInput = parseInt($("#number-input").val());
-// const numberInput = document.getElementById("number-input").value;
-
-const beep = "beep!";
-const boop = "boop!";
-const neighbor = "wont you be my neighbor?";
+//                            Business Logic:
+const beep = "Beep!";
+const boop = "Boop!";
+const neighbor = "Wont you be my neighbor?";
 
 let indexNumber = function (inputText) {
   let numberArray = [];
   for (let i = 0; i <= inputText; i++) {
     numberArray.push(i.toString());
   }
-  debugger
+
   let textOutput = numberArray.map(function (input) {
     if (input.includes("2" && "3")) {
       return input = neighbor;
@@ -31,25 +25,12 @@ let indexNumber = function (inputText) {
       return input = beep;
     } else {
       return input;
-
-      // if (input.includes("1")) {
-      //   return input = beep;
-      // } else if (input.includes("1" && "2")) {
-      //   return input = boop;
-      // } else if (input.includes("2")) {
-      //   return input = boop;
-      // } else if (input.includes("1" && "3")) {
-      //   return input = neighbor;
-      // } else if (input.includes("2" && "3")) {
-      //   return input = neighbor;
-      // } else if (input.includes("3")) {
-      //   return input = neighbor;
-      // } else {
-      //   return input;
     }
   });
   return textOutput;
 };
+
+//                                    UI Logic:
 
 $(document).ready(function () {
   $("#form").submit(function (event) {
@@ -60,3 +41,29 @@ $(document).ready(function () {
     $(".output").show();
   });
 });
+
+// Prior Attempts:
+
+// if (input.includes("1")) {
+  //   return input = beep;
+  // } else if (input.includes("1" && "2")) {
+    //   return input = boop;
+    // } else if (input.includes("2")) {
+      //   return input = boop;
+      // } else if (input.includes("1" && "3")) {
+        //   return input = neighbor;
+        // } else if (input.includes("2" && "3")) {
+          //   return input = neighbor;
+          // } else if (input.includes("3")) {
+            //   return input = neighbor;
+            // } else {
+              //   return input;
+
+
+              // $(document).ready(function () {
+              //   $("#number-input").submit(function (event)) {
+              // event.preventDefault();
+
+              //     ().onclick()
+              // const numberInput = parseInt($("#number-input").val());
+              // const numberInput = document.getElementById("number-input").value;
